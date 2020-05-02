fe953fec004be5cc9aba9d38b16f5026cab6aa3d
Simon Sedlacek <saimon.sedlacek@gmail.com>
2020-05-03 01:23:01 +0200

    sniff sniff

--- src/sniffer.cpp
+++ src/sniffer.cpp
@@ -1,10 +1,11 @@
 /**
  * @file   sniffer.cpp
  * @author Simon Sedlacek, xsedla1h
- * @brief 
- */
+ * @brief  This module implements the core sniffer functions.
+ */ 
 
 #include "sniffer.hpp"
+
 
 int sniff_init(char *interface, unsigned *port, unsigned *n_packets, bool tcp, bool udp) {
 
@@ -35,6 +36,11 @@ int sniff_init(char *interface, unsigned *port, unsigned *n_packets, bool tcp, b
         mask = 0;
     }
 
+    //TODO tcp/udp filter
+    if (tcp != udp) {
+        //setup filter
+    }
+
     /* Filter the traffic by port if specified... */
     if (port != nullptr) {
         struct bpf_program fp;
@@ -55,14 +61,14 @@ int sniff_init(char *interface, unsigned *port, unsigned *n_packets, bool tcp, b
     }
 
     /* Start the actual sniffing */
-    if (sniff(handle, n_packets, tcp, udp) != SUCCESS) {
+    if (sniff(handle, n_packets) != SUCCESS) {
         return ERROR_SNIFFER;
     }
 
     return SUCCESS;
 }
 
-int sniff(pcap_t *handle, unsigned *n_packets, bool tcp, bool udp) {
+int sniff(pcap_t *handle, unsigned *n_packets) {
     int retval;
     u_char *user = NULL; //TODO: possibly useful for storing flags??
     unsigned packet_count = 1;
@@ -81,17 +87,199 @@ int sniff(pcap_t *handle, unsigned *n_packets, bool tcp, bool udp) {
             pcap_perror(handle, "Error: ");
 
         } else if (retval == PCAP_ERROR_BREAK) {
+            std::cerr << "PCAP_ERROR_BREAK\n";
             break;
             // TODO: what to do here...
         }
+
+
+        /* Print out the content of packet_data */
     }
 
     pcap_close(handle);
     return SUCCESS;
 }
 
-
 void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
-    std::cout << "Got a packet of length " << header->len << std::endl;
-    std::cout << packet << std::endl;
+    if (args) {} /* eliminate the unused parameter warning... */
+    int retval;
+    const struct ether_header *eth_header;
+    pckt_data packet_data;
+
+    packet_data.header = header;
+    packet_data.packet = packet;
+
+    eth_header = (struct ether_header *)(packet); /* get the ethernet header */
+    u_short type = ntohs(eth_header->ether_type); /* get the ethernet type */
+
+    if (type == ETHERTYPE_IP) {
+        retval = process_ipv4(packet + sizeof(ether_header), &packet_data);
+
+    } else if (type == ETHERTYPE_IPV6) {
+        retval = process_ipv6(packet + sizeof(ether_header), &packet_data);
+
+    } else {
+        return;
+    }
+
+    if (retval != 0) {
+        return; //TODO
+    }
+
+    print_current_packet_data(packet_data);
+    return;
+}
+
+int process_ipv4(const u_char *packet, pckt_data *packet_data) {
+    int retval;
+    const struct ip *ip_hdr;
+    packet_data->ipv = IP_VER4;
+
+    /* Extract the ip packet */
+    ip_hdr = (struct ip *)(packet);
+
+    /* Store the source and destination ip_address */
+    packet_data->ip.ipv4_src = ip_hdr->ip_src;
+    packet_data->ip.ipv4_dst = ip_hdr->ip_dst;
+
+    /* Determine whether the underlying packet is tcp or udp and trim off the ip
+     * header and options.. */
+    int offset = ip_hdr->ip_hl - 5;
+    if (ip_hdr->ip_p == PRTCL_TCP) {
+        retval = process_tcp((const u_char *)ip_hdr + sizeof(struct ip) + offset,
+                packet_data);
+
+    } else if (ip_hdr->ip_p == PRTCL_UDP){
+        retval = process_udp((const u_char *)ip_hdr + sizeof(struct ip) + offset,
+                packet_data);
+        
+    } else {
+        retval = -1; /* Unsupported protocol */
+    }
+
+    return retval;
+}
+
+int process_ipv6(const u_char *packet, pckt_data *packet_data) {
+    int retval;
+    const struct ip6_hdr *ip_hdr;
+    packet_data->ipv = IP_VER6;
+
+    ip_hdr = (struct ip6_hdr *)(packet);
+
+    /* Store the source and destination ip_address */
+    packet_data->ip.ipv6_src = ip_hdr->ip6_src;
+    packet_data->ip.ipv6_dst = ip_hdr->ip6_dst;
+
+    /* Determine the type of the underlying packet */
+    if (ip_hdr->ip6_ctlun.ip6_un1.ip6_un1_nxt == PRTCL_TCP) {
+        retval = process_tcp((const u_char *)ip_hdr + sizeof(struct ip6_hdr),
+                packet_data);
+
+    } else if (ip_hdr->ip6_ctlun.ip6_un1.ip6_un1_nxt == PRTCL_UDP){
+        retval = process_udp((const u_char *)ip_hdr + sizeof(struct ip6_hdr),
+                packet_data);
+        
+    } else {
+        retval = -1; /* Unsupported protocol */
+    }
+
+    return retval;
+}
+
+int process_udp(const u_char *packet, pckt_data *packet_data) {
+    int retval = SUCCESS;
+
+    /* Get the udp header */
+    const struct udphdr *udp_hdr;
+    udp_hdr = (struct udphdr *)(packet);
+
+    /* Get packet info */
+    packet_data->tcp_udp = PRTCL_UDP;
+    packet_data->src_port = udp_hdr->source;
+    packet_data->dst_port = udp_hdr->dest;
+
+    return retval;
+}
+
+int process_tcp(const u_char *packet, pckt_data *packet_data) {
+    int retval = SUCCESS;
+
+    /* Get the tcp header */
+    const struct tcphdr *tcp_hdr;
+    tcp_hdr = (struct tcphdr *)(packet);
+
+    /* Get packet info */
+    packet_data->tcp_udp = PRTCL_TCP;
+    packet_data->src_port = tcp_hdr->source;
+    packet_data->dst_port = tcp_hdr->dest;
+
+    return retval;
+}
+
+void print_current_packet_data(const pckt_data packet_data) {
+    //std::cout << packet_data.header->ts.tv_sec;
+
+    /* Print the adresses and port numbers */
+    if (packet_data.ipv == IP_VER4) {
+        printf("%s : %d > ", inet_ntoa(packet_data.ip.ipv4_src), packet_data.src_port);
+        printf("%s : %d\n", inet_ntoa(packet_data.ip.ipv4_dst), packet_data.dst_port);
+
+    } else if (packet_data.ipv == IP_VER6) {
+        /* Source address and port */
+        for (int i = 0; i < 7; i++)
+            printf("%x:", packet_data.ip.ipv6_src.s6_addr16[i]);
+        printf("%x : %d > ", packet_data.ip.ipv6_src.s6_addr16[7], packet_data.src_port);
+
+        /* Destination address and port */
+        for (int i = 0; i < 7; i++)
+            printf("%x:", packet_data.ip.ipv6_dst.s6_addr16[i]);
+        printf("%x : %d\n", packet_data.ip.ipv6_dst.s6_addr16[7], packet_data.dst_port);
+
+    } else {
+        return; /* This should not happen... */
+    }
+
+    /* Now print the actual packet data... */
+    std::string output; /* Stores the ascii representation of the bytes... */
+    output.clear();
+
+    for (unsigned i = 0; i < packet_data.header->caplen; i++) {
+        if (i % 8 == 0 && i % 16 != 0) {
+            /* Separate the output by 8 octets */
+            printf(" ");
+            output += ' ';
+        }
+
+        if (i % 16 == 0) {
+            if (i != 0) {
+                printf(" %s\n", output.c_str());
+                output.clear();
+            }
+            printf("0x%04x:  ", i);
+        }
+
+        /* Get the next octet */
+        int c = packet_data.packet[i];
+        /* Append it to the output string */
+        if (isgraph(c) || c == ' ')
+            output += c;
+        else
+            output += '.';
+        
+        printf("%02x ", c); /* Print the hexa value */
+    }
+
+    /* Print the last row */
+    int len = output.length();
+    if (len != 0) {
+        if (len > 8) printf("   ");
+        for (int i = len; i < 16; i++) {
+            if (i == 8) printf(" ");
+            printf("   ");
+        }
+        printf(" %s\n", output.c_str());
+    }
+    printf("\n");
+
 }
--- src/sniffer.hpp
+++ src/sniffer.hpp
@@ -1,17 +1,51 @@
 /**
  * @file   sniffer.hpp
  * @author Simon Sedlacek, xsedla1h
- * @brief 
+ * @brief  sniffer module header file
  */
 
 #include <iostream>
 #include <pcap.h>
+#include <netinet/in.h>
+#include <netinet/ether.h>
+#include <netinet/ip.h>
+#include <netinet/ip6.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <net/ethernet.h>
+#include <arpa/inet.h>
 
 #include "resources.hpp"
 
+#define PRTCL_TCP 6
+#define PRTCL_UDP 17 
+#define PRTCL_ICMP 1 
+
+#define IP_VER4 4
+#define IP_VER6 6
+
+/**
+ * @brief This structure holds the information about the currently processed packet and
+ * is used to easily print the packet data and metadata once the processing is finished
+ */
+typedef struct packet_info {
+    const struct pcap_pkthdr *header;
+    char ipv; /* Either IP_VER4 or IP_VER6 */
+    char tcp_udp; /* Either PRTCL_TCP or PRTCL_UDP */
+    uint16_t src_port;
+    uint16_t dst_port;
+
+    union { /* The ip addresses.. */
+        struct in_addr ipv4_src, ipv4_dst;
+        struct in6_addr ipv6_src, ipv6_dst;
+    } ip;
+
+    const u_char *packet; /* The whole packet */
+} pckt_data;
+
 /**
  * @brief This function opens the desired device, sets up the necessary resources and
- * filters in order to start the sniffing process..
+ * packet filters in order to start the sniffing process..
  */
 int sniff_init(char *interface, unsigned *port, unsigned *n_packets, bool tcp, bool udp);
 
@@ -19,9 +53,40 @@ int sniff_init(char *interface, unsigned *port, unsigned *n_packets, bool tcp, b
  * @brief This function is the top-level function for the sniffing process, the sniffing
  * "loop" with pcap_loop() is situated in here
  */
-int sniff(pcap_t *handle, unsigned *n_packets, bool tcp, bool udp);
+int sniff(pcap_t *handle, unsigned *n_packets);
 
 /**
  * @brief The callback function for pcap_loop() which processes the received packets
  */
 void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
+
+/**
+ * @brief This function processes the ipv4 header and passes on the stripped packet
+ * Additionally, the source and destination ipv4 addresses are stored in the
+ * packet_data structure
+ */
+int process_ipv4(const u_char *packet, pckt_data *packet_data);
+
+/**
+ * @brief This function processes the ipv6 header and passes on the stripped packet.
+ * Additionally, the source and destination ipv6 addresses are stored in the
+ * packet_data structure
+ */
+int process_ipv6(const u_char *packet, pckt_data *packet_data);
+
+/**
+ * @brief This function processes the udp header and stores the source and destination
+ * ports to the packet_data sturcture
+ */
+int process_udp(const u_char *packet, pckt_data *packet_data);
+
+/**
+ * @brief This function processes the tcp header and stores the source and destination
+ * ports to the packet_data sturcture
+ */
+int process_tcp(const u_char *packet, pckt_data *packet_data);
+
+/**
+ * @brief Prints the formated form of packet_data
+ */
+void print_current_packet_data(const pckt_data packet_data);
